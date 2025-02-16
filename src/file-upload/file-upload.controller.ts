53fd1df782ec6f1d6be7cfeb260b08c8d4972a49
nhathv <nhathv.21it@gmail.com>
2025-02-16 12:45:50 +0700

    feat: enhance file upload endpoint with Swagger documentation for multipart/form-data

--- src/file-upload/file-upload.controller.ts
+++ src/file-upload/file-upload.controller.ts
@@ -7,6 +7,7 @@ import {
 import { FileInterceptor } from '@nestjs/platform-express';
 import { FileUploadService } from './file-upload.service';
 import { ResponeMessage } from 'src/decorator/customize';
+import { ApiBody, ApiConsumes } from '@nestjs/swagger';
 
 @Controller('file-upload')
 export class FileUploadController {
@@ -14,6 +15,18 @@ export class FileUploadController {
 
   @Post('upload')
   @ResponeMessage('File uploaded successfully')
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
   @UseInterceptors(FileInterceptor('file'))
   uploadFile(@UploadedFile() file: Express.Multer.File) {
     return this.fileUploadService.handleFileUpload(file);
