f9adcdf59a6fecdaf31d2dac020395c891306afc
nhathv <nhathv.21it@gmail.com>
2025-02-16 14:58:00 +0700

    feat: add support for uploading multiple files with validation in file upload service

--- src/file-upload/file-upload.controller.ts
+++ src/file-upload/file-upload.controller.ts
@@ -3,8 +3,9 @@ import {
   Post,
   UseInterceptors,
   UploadedFile,
+  UploadedFiles,
 } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { FileUploadService } from './file-upload.service';
 import { ResponeMessage } from 'src/decorator/customize';
 import { ApiBody, ApiConsumes } from '@nestjs/swagger';
@@ -30,5 +31,29 @@ export class FileUploadController {
   @UseInterceptors(FileInterceptor('file'))
   uploadFile(@UploadedFile() file: Express.Multer.File) {
     return this.fileUploadService.handleFileUpload(file);
+  }
+
+  @Post('upload-multiple')
+  @ResponeMessage('Multiple files uploaded successfully')
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        files: {
+          type: 'array',
+          items: {
+            type: 'string',
+            format: 'binary',
+          },
+        },
+      },
+    },
+  })
+  @ResponeMessage('Multiple files uploaded successfully')
+  @UseInterceptors(FilesInterceptor('files', 5))
+  uploadMultipleFiles(@UploadedFiles() files: Express.Multer.File[]) {
+    console.log(124);
+    return this.fileUploadService.uploadFiles(files);
   }
 }
--- src/file-upload/file-upload.service.ts
+++ src/file-upload/file-upload.service.ts
@@ -21,4 +21,25 @@ export class FileUploadService {
 
     return { filePath: file.path };
   }
+  async uploadFiles(files: Express.Multer.File[]) {
+    if (!files || files.length === 0) {
+      throw new BadRequestException('no file uploaded');
+    }
+
+    const allowedMimeTypes = ['image/jpeg', 'image/png', 'application/pdf'];
+    const maxSize = 5 * 1024 * 1024;
+    const result = [];
+
+    for (const file of files) {
+      if (!allowedMimeTypes.includes(file.mimetype)) {
+        throw new BadRequestException('invalid file type');
+      }
+
+      if (file.size > maxSize) {
+        throw new BadRequestException('file is too large!');
+      }
+      result.push(file.path);
+    }
+    return { filePath: result };
+  }
 }
